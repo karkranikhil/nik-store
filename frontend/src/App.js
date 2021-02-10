7280f077f90d70073da16569ad308daba5ab6ca2
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-02-10 16:20:29 +1100

    fontawesome, header and footer align

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
-const App = ()=> {
-  return <div className='App'>Welcome</div>
+import { Container } from 'react-bootstrap'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+      <Container>
+        <main className='py-3'>Welcome</main>
+      </Container>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default App
