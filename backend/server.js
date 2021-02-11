6394122d2f63398add9d4876eca78c2459fa693f
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-02-11 18:39:58 +1100

    mongoDb setup and connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+import products from './data/products.js'
+
 dotenv.config()
+
+connectDB()
+
 const app = express()
 app.get('/', (req, res) => {
   res.send('API is running')
@@ -16,5 +23,5 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} port ${PORT}`)
+  console.log(`server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold)
 )
