83667f520343ef3ade0c1baa8d1ecbc09d3a8e38
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-02-13 15:23:09 +1100

    error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,22 +6,29 @@ const router = express.Router()
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public route
-router.get('/',asyncHandler(async(req, res) => {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
-}))
+  })
+)
 
 // @desc Fetch Single product
 // @route GET /api/products
 // @access Public route
-router.get('/:id', asyncHandler(async(req, res) => {
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    console.log('called')
     const product = await Product.findById(req.params.id)
-    if(product){
-        res.json(product)
+    if (product) {
+      res.json(product)
     } else {
-        res.status(404).json({message:'Product not found'})
+      res.status(404)
+      throw new Error('Product not found')
     }
-    
-}))
+  })
+)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 import connectDB from './config/db.js'
 
 // import products from './data/products.js'
@@ -16,8 +18,14 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold)
+  console.log(
+    `server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold
+  )
 )
