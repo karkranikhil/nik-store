044984438387888c8a2da6f062e42b89b38f04ce
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-02-13 17:04:56 +1100

    each product redux

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -14,3 +14,21 @@ export const productListReducer = (state = initialState, action) => {
         return state
   }
 }
+
+const initialDetailsState = {
+    product:{
+        reviews:[]
+    }
+}
+export const productDetailsReducer = (state = initialDetailsState, action) => {
+    switch (action.type) {
+      case PRODUCT_CONSTANTS.PRODUCT_DETAILS_REQUEST:
+        return { ...state, loading: true}
+      case PRODUCT_CONSTANTS.PRODUCT_DETAILS_SUCCESS:
+          return { ...state, loading: false, product:action.payload }
+      case PRODUCT_CONSTANTS.PRODUCT_DETAILS_FAIL:
+          return { ...state, loading: false, error:action.payload }
+      default:
+          return state
+    }
+  }
