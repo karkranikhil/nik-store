e832aec082c9bd964cfbf19ce89d1319e9ff27e8
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-02-17 11:18:06 +1100

    moving logic to controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
 const router = express.Router()
+import  {getProducts, getProductById} from '../controllers/productController.js'
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public route
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-// @desc Fetch Single product
-// @route GET /api/products
-// @access Public route
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    console.log('called')
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
